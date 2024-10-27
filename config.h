3fa49259d704a787953b2dcde477e3f60ae1b8be
David <david19kerrigan@gmail.com>
2024-10-27 12:40:13 -0400

    auto commit

--- config.h
+++ config.h
@@ -96,23 +96,25 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-"#181818",
-"#ab4642",
-"#a1b56c",
-"#f7ca88",
-"#7cafc2",
-"#ba8baf",
-"#86c1b9",
-"#d8d8d8",
+/* 8 normal colors */
+	[0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	[1] = "#cc241d", /* red     */
+	[2] = "#98971a", /* green   */
+	[3] = "#d79921", /* yellow  */
+	[4] = "#458588", /* blue    */
+	[5] = "#b16286", /* magenta */
+	[6] = "#689d6a", /* cyan    */
+	[7] = "#a89984", /* white   */
 
-"#585858",
-"#ab4642",
-"#a1b56c",
-"#f7ca88",
-"#7cafc2",
-"#ba8baf",
-"#86c1b9",
-"#f8f8f8"
+	/* 8 bright colors */
+	[8]  = "#928374", /* black   */
+	[9]  = "#fb4934", /* red     */
+	[10] = "#b8bb26", /* green   */
+	[11] = "#fabd2f", /* yellow  */
+	[12] = "#83a598", /* blue    */
+	[13] = "#d3869b", /* magenta */
+	[14] = "#8ec07c", /* cyan    */
+	[15] = "#ebdbb2", /* white   */
 };
 
 /*
